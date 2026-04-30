7a3669e0a964f09c83630bc1b9a85e9c9598b835
TalkoDA <azzi2710@gmail.com>
2026-04-30 09:01:57 +0200

    Added main.R

---
+++ main.R
@@ -0,0 +1,39 @@
+source("src/nn/base_layer.R")
+source("src/nn/dense_layer.R")
+source("src/nn/relu.R")
+source("src/nn/mse.R")
+source("src/nn/network.R")
+
+X <- matrix(c(0,0,
+              0,1,
+              1,0.
+              1,1), nclo=2, byrow=TRUE0)
+
+Y <- matrix(c(0,
+              1,
+              1,
+              0), ncol=1)
+
+net <- network$new()
+net$add(DenseLayer$new(input_size=2, output_size=4))
+net$add(ActivationReLU$new())
+net$add(DenseLayer$new(input_size=4, output_size=1))
+
+loss_fn <- MSELoss$new()
+learning_rate <- 0.1
+epochs <- 1000
+
+cat("Starting training...\n")
+for (epoch in 1:epochs) {
+  prediction <- net$forward(X)
+  loss <- loss_fn$forward(predictions, Y)
+  grad <- loss_fn$backward(predictions, Y)
+  net$backward(grad, learning_rate)
+
+  if (epoch %% 100 == 0) {
+    cat(sprintf("Epoch: %d | Loss %f\n", epoch, loss))
+  }
+
+cat("Training complete. Final Predictions:")
+print(net$forward(X))
+}
